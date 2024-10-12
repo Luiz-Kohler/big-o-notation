56db725f97b77a5de6639ccb08f136c54fa895bb
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-10-12 02:58:12 -0300

    add square time complexity examples

---
+++ big-o-complexity-examples/5. square-time-complexity.js
@@ -0,0 +1,35 @@
+// O(n^2) Square/Quadratic Time Complexity
+
+function createMatrix(size) {
+    var matrix = [];
+    for (let column = 0; column < size; column++) {
+        matrix[column] = [];
+
+        for (let row = 0; row < size; row++) 
+            matrix[column][row] = `${column} ${row}`;
+    }
+
+    return matrix;
+}
+
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped; 
+
+    for (let i = 0; i < n - 1; i++) {
+        swapped = false;
+
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+
+    return arr;
+}
